e9ccc2a2c94e16ce74526ad48b4cdcd9261b0435
Clement Adjavon <aclement400@gmail.com>
2026-02-27 10:41:39 +0000

    added view, urls and template files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
+
+
+def list_books(request):
+    books = Book.objects.all()
+    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output, content_type="text/plain")
+
+# function - template based relations/view.py
+
+from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+
+
+
+
+# class based - relationship_app/views.py
+
+from django.views.generic import DetailView
+from .models import Library
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+
+# Variation
+
+from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 
@@ -15,5 +52,6 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
 
 
